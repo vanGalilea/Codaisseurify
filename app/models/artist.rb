cfeba062df6ad04e6dade79921b83b97cf7b7717
vanGalilea <z.galili@icloud.com>
2017-05-27 09:29:01 +0200

    created album model

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,5 @@
 class Artist < ApplicationRecord
-  has_many :songs, dependent: :destroy
+  has_many :albums, dependent: :destroy
 
   validates :name, presence: true, length: {maximum: 50}
   validates :description, presence: true, length: {maximum: 500}
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,7 @@
 class Song < ApplicationRecord
-  belongs_to :artist
+  belongs_to :album
 
   validates :name, presence: true, length: {maximum: 50}
   validates :url, presence: true
+
 end
