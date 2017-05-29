f803ec6569c4cdfda9064f280b6411bd793fd687
vanGalilea <z.galili@icloud.com>
2017-05-29 16:12:48 +0200

    views changed due to fav list ans styling

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,20 +1,26 @@
 class FavouritesController < ApplicationController
   # before_action :authenticate_user!, except: [:show]
-  before_action :set_song
 
   def new
+    set_song_id
     @favourite = Favourite.new(song: @song ,profile: current_user.profile)
     @favourite.save
     redirect_to root_path, notice: "Song added to your Fav. list!"
   end
 
   def destroy
+    set_song
+    @song = Song.find(params[:id])
     @favourite = Favourite.find_by(song_id: @song.id)
     @favourite.destroy
     redirect_to root_path, notice: "Song deleted from your Fav. list!"
   end
 
   private
+    def set_song_id
+      @song = Song.find(params[:song_id])
+    end
+
     def set_song
       @song = Song.find(params[:id])
     end
